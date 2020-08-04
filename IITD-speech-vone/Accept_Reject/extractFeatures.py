4abfa2be43831f20dcdcbb90c48d7de78f5aba18
Prashitraj <40001918+Prashitraj@users.noreply.github.com>
2020-08-04 10:45:08 +0530

    Removed chunking issue
    Previously the feature extraction uses chunking to extract data from audio. In this commit that feature is removed as the models are trained without chunking.

--- IITD-speech-vone/Accept_Reject/extractFeatures.py
+++ IITD-speech-vone/Accept_Reject/extractFeatures.py
@@ -1,5 +1,5 @@
 from pyAudioAnalysis import audioBasicIO
-from pyAudioAnalysis import ShortTermFeatures
+from pyAudioAnalysis import audioFeatureExtraction
 from statistics import mean, stdev
 import numpy as np
 import csv
@@ -16,71 +16,71 @@ def get_features(input_file):
 	'''
 	data, samplerate = sf.read(input_file)
 	l1 = []
-	[Fs, x] = audioBasicIO.read_audio_file(input_file)
-	F,f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.05*Fs, 0.025*Fs)
+	[Fs, x] = audioBasicIO.readAudioFile(input_file)
+	F,f_names = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.05*Fs, 0.025*Fs)
 	k = 0
-	while k < len(F[0]):
-		l = []
-		for j in range(34):
-			l.append(np.percentile(F[j, k:k+399], 25))
-			l.append(np.percentile(F[j, k:k+399], 50))
-			l.append(np.percentile(F[j, k:k+399], 75))
-			l.append(np.percentile(F[j, k:k+399], 95))
-		l.append(len(F[j])/399)
-		l.append('1')	#default label is reject
-		l1.append(l)
-		k = k + 399
+	l = []
+	for j in range(34):
+		l.append(np.percentile(F[j, :], 25))
+		l.append(np.percentile(F[j, :], 50))
+		l.append(np.percentile(F[j, :], 75))
+		l.append(np.percentile(F[j, :], 95))
+	
+	l.append(len(F[j])/399)
+	if fname.startswith("acc"):
+		l.append(2)
+	else:
+		l.append(1)
+	l1.append(l)
 	return l1
-
-
 ''' this function will extract the features from a wav audio file and save it to a csv file'''
 
-def generateFeaturesData(outputData):
-	l1 =[]
-	for i in range(1, 1501, 1):
-		print("Rej",i)
-		try:
-			[Fs, x] = audioBasicIO.read_audio_file("rej_" + str(i) + ".wav")
-			F,f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.05*Fs, 0.025*Fs)
-		except:
-			continue
-		k = 0
-		while k<len(F[0]):
-			l = []
-			for j in range(34):
-				l.append(np.percentile(F[j, k:k+399], 25))
-				l.append(np.percentile(F[j, k:k+399], 50))
-				l.append(np.percentile(F[j, k:k+399], 75))
-				l.append(np.percentile(F[j, k:k+399], 95))
+# def generateFeaturesData(outputData):
+# 	l1 =[]
+# 	for i in range(1, 1501, 1):
+# 		print("Rej",i)
+# 		try:
+# 			[Fs, x] = audioBasicIO.read_audio_file("rej_" + str(i) + ".wav")
+# 			F,f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.05*Fs, 0.025*Fs)
+# 		except:
+# 			continue
+# 		k = 0
+# 		while k<len(F[0]):
+# 			l = []
+# 			for j in range(34):
+# 				l.append(np.percentile(F[j, k:k+399], 25))
+# 				l.append(np.percentile(F[j, k:k+399], 50))
+# 				l.append(np.percentile(F[j, k:k+399], 75))
+# 				l.append(np.percentile(F[j, k:k+399], 95))
 			
-			l.append(len(F[j])/399)
-			l.append(1)
-			l1.append(l)
-			k = k + 399
-	for i in range(1, 1501, 1):
-		print("Acc",i)
-		try:
-			[Fs, x] = audioBasicIO.read_audio_file("acc_" + str(i) + ".wav")
-			F,f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.05*Fs, 0.025*Fs)
-		except:
-			continue
-		k = 0
-		while k < len(F[0]):
-			l = []
-			for j in range(34):
-				l.append(np.percentile(F[j, k:k+399], 25))
-				l.append(np.percentile(F[j, k:k+399], 50))
-				l.append(np.percentile(F[j, k:k+399], 75))
-				l.append(np.percentile(F[j, k:k+399], 95))
+# 			l.append(len(F[j])/399)
+# 			l.append(1)
+# 			l1.append(l)
+# 			k = k + 399
+# 	for i in range(1, 1501, 1):
+# 		print("Acc",i)
+# 		try:
+# 			[Fs, x] = audioBasicIO.read_audio_file("acc_" + str(i) + ".wav")
+# 			F,f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.05*Fs, 0.025*Fs)
+# 		except:
+# 			continue
+# 		k = 0
+# 		while k < len(F[0]):
+# 			l = []
+# 			for j in range(34):
+# 				l.append(np.percentile(F[j, k:k+399], 25))
+# 				l.append(np.percentile(F[j, k:k+399], 50))
+# 				l.append(np.percentile(F[j, k:k+399], 75))
+# 				l.append(np.percentile(F[j, k:k+399], 95))
 			
-			l.append(len(F[j])/399)
-			l.append(2)
-			l1.append(l)
-			k = k + 399
+# 			l.append(len(F[j])/399)
+# 			l.append(2)
+# 			l1.append(l)
+# 			k = k + 399
 
-	with open(outputData, "w") as f:
-		writer = csv.writer(f)
-		writer.writerows(l1)
+# 	with open(outputData, "w") as f:
+# 		writer = csv.writer(f)
+# 		writer.writerows(l1)
 
-#get_features('ex1.wav')
-#generateFeaturesData("full_data.csv")
+# #get_features('ex1.wav')
+# #generateFeaturesData("full_data.csv")
